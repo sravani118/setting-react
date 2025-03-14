fd405aca3bd0bcb24ca4be4096940b25260f808d
penumajji sravani <penumajjisravani698@gmail.com>
2025-03-14 13:53:13 +0530

    setting react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My Name: Sravani</h1>
+      <p>EMAIL address: sravani18@example.com</p>
+      <p>age: 18</p>
+      <p>contact no: 8919274673</p>
+    </div>
+  );
+};
+
+export default Home;
